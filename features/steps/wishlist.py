06c6c78a20eec57284f623016544e9fd6ecf2387
JuliaCar <mjp54321@gmail.com>
2020-10-01 14:16:33 -0700

    TCs for wishlist

---
+++ features/steps/wishlist.py
@@ -0,0 +1,36 @@
+from behave import when, then
+
+
+@when('Add to wishlist by clicking on Heart icon')
+def wishlist_click_icon(context):
+    context.app.wishlist_page.click_heart_icon()
+
+
+@then('Move to the wishlist page')
+def move_wishlist_page(context):
+    context.app.wishlist_page.click_heart_icon()
+
+
+@then('Verify that user can see {product}')
+def user_see_correct_product_name(context, product):
+    context.app.wishlist_page.user_see_correct_product_name(product)
+
+
+@when('Open another product page')
+def another_product_page(context):
+    context.app.wishlist_page.another_product_page()
+
+
+@then('Verify user can see {amount_logos} social logos')
+def wishlist_social_logos_present(context, amount_logos):
+    context.app.wishlist_page.wishlist_social_logos_present(amount_logos)
+
+
+@then('Delete item from wishlist')
+def delete_item_wishlist(context):
+    context.app.wishlist_page.delete_item_wishlist()
+
+
+@then('Verify {no_item_wishlist} is shown')
+def no_item_wishlist_text(context, no_item_wishlist):
+    context.app.wishlist_page.no_item_wishlist_text(no_item_wishlist)
--- pages/wishlist_page.py
+++ pages/wishlist_page.py
@@ -5,14 +5,33 @@ from time import sleep
 
 class Wishlist(Page):
     PRODUCT_NAME = (By.CSS_SELECTOR, "td.product-name")
-    PHONE_CATEGORY = (By.ID, "#menu-item-469")
-    PHONE = (By.ID, "menu-item-469")
+    SECOND_PRODUCT_ADD = (By.CSS_SELECTOR, "span.product-title")
+    PIC = (By.CSS_SELECTOR, "img.wp-post-image.skip-lazy")
+    HEART_ICON = (By.CSS_SELECTOR, "i.icon-heart")
+    SOCIAL_LOGOS = (By.CSS_SELECTOR, "div.social-icons.share-icons a")
+    REMOVE_ITEM = (By.CSS_SELECTOR, "a.remove_from_wishlist")
+    EMPTY_WISHLIST = (By.CSS_SELECTOR, "td.wishlist-empty")
 
     def user_see_correct_product_name(self, product):
         self.verify_text(product, *self.PRODUCT_NAME)
 
     def another_product_page(self):
-        self.click(*self.PHONE_CATEGORY)
+        product = self.find_elements(*self.SECOND_PRODUCT_ADD)[5]
+        product.click()
 
 
+    def click_heart_icon(self):
+        heart_icon = self.find_element(*self.HEART_ICON)
+        self.actions.move_to_element(heart_icon)
+        self.actions.perform()
+        self.click(*self.HEART_ICON)
 
+    def wishlist_social_logos_present(self, amount_logos):
+        logos = self.find_elements(*self.SOCIAL_LOGOS)
+        assert int(amount_logos) == len(logos)
+
+    def delete_item_wishlist(self):
+        self.click(*self.REMOVE_ITEM)
+
+    def no_item_wishlist_text(self, no_item_wishlist):
+        self.verify_text(no_item_wishlist, *self.EMPTY_WISHLIST)
